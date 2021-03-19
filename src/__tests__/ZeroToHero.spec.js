4f8e21926676a9ac118ab53bade7f0c5082488c6
it-baymax-23 <xuzhihao9423@outlook.com>
2021-03-19 14:15:19 -0400

    async promise testing

--- src/__tests__/ZeroToHero.spec.js
+++ src/__tests__/ZeroToHero.spec.js
@@ -67,4 +67,44 @@ test('spying using mockImplementation', () => {
   const spy = jest.spyOn(pizza, 'name');
   spy.mockImplementation(n => 'Crazy pizza!');
   expect(pizza.name('Cheese')).toBe('Crazy pizza!');
+  spy.mockRestore();
+  expect(pizza.name('Cheese')).toBe('Pizza name: Cheese');
+});
+
+test('pizza returns new york last', () => {
+  const pizza1 = pizzas[0];
+  const pizza2 = pizzas[1];
+  const pizza3 = pizzas[2];
+  const pizza = jest.fn(currentPizza => currentPizza.name);
+
+  pizza(pizza1);
+  pizza(pizza2);
+  pizza(pizza3);
+
+  expect(pizza).toHaveLastReturnedWith('New York Pizza');
+});
+
+test('pizza data has new york pizza and matches as an object', () => {
+  const newYorkPizza = {
+    id: 3,
+    name: "New York Pizza",
+    image: "/images/ny-pizza.jpg",
+    desc: "New York-style pizza has slices that are large and wide with a thin crust that is foldable yet crispy. It is traditionally topped with tomato sauce and mozzarella cheese.",
+    price: 8
+  };
+  expect(pizzas[2]).toMatchObject(newYorkPizza);
+});
+
+test('expect a promise to resolve', async () => {
+  const user = {
+    getFullName: jest.fn(() => Promise.resolve('Karl Hadwen')),
+  };
+  await expect(user.getFullName('Karl Hadwen')).resolves.toBe('Karl Hadwen');
+});
+
+test('expect a promise to reject', async () => {
+  const user = {
+    getFullName: jest.fn(() => Promise.reject(new Error('Something went terribly wrong.'))),
+  };
+  await expect(user.getFullName('Karl Hadwen')).rejects.toThrow('Something went terribly wrong.');
 });
